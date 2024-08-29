9196913067c68d80185ab82a5ac801992e31afcd
unknown <sunilrm2025@gmail.com>
2024-08-29 12:08:20 +0530

    Revert "final commit"
    This reverts commit dfcc589c2885ee2f88272f5270f62c03fecc31bd.

--- src/main/java/in/ashokit/Robot.java
+++ src/main/java/in/ashokit/Robot.java
@@ -7,7 +7,5 @@ public class Robot {
 
 	public Robot() {
 		System.out.println("Robot :: Constructor executed..");
-		System.out.println("Robot again commiting  :: Constructor executed..");
-
 	}
 }
